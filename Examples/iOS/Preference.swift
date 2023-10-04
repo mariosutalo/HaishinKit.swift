2b140056a11a4f3a9f774a34d1f1336f662c1823
Mario Šutalo <mario.sutalo@gmail.com>
2023-10-04 07:49:42 +0200

    - initial commit

--- Examples/iOS/Preference.swift
+++ Examples/iOS/Preference.swift
@@ -1,6 +1,6 @@
 struct Preference {
     static var defaultInstance = Preference()
 
-    var uri: String? = "rtmp://192.168.1.9/live"
-    var streamName: String? = "live"
+    var uri: String? = "rtmp://streamer4.rolla.app/rc/"
+    var streamName: String? = "test"
 }
